92d85819558c3c5531b7835599a41ee77e9a4544
2391134843 <2391134843@qq.com>
2018-05-30 22:06:40 +0800

    显示hdfs文件路径，权限，大小，穿件时间..信息

---
+++ HDFS/HDFSapi.java
@@ -0,0 +1,39 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+public class HDFSapi{
+/**
+* 显示指定文件的信息
+*/
+public static void ls(Configuration conf, String remoteFilePath) throws IOException {
+	FileSystem fs = FileSystem.get(conf);
+	Path remotePath = new Path(remoteFilePath);
+	FileStatus[] fileStatuses = fs.listStatus(remotePath);
+	for (FileStatus s : fileStatuses) {
+	System.out.println("路径: " + s.getPath().toString());
+	System.out.println("权限: " + s.getPermission().toString());
+	System.out.println("大小: " + s.getLen());
+	/* 返回的是时间戳,转化为时间日期格式 */
+	Long timeStamp = s.getModificationTime();
+	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	String date = format.format(timeStamp);
+	System.out.println("时间: " + date);
+	}
+	fs.close();
+}
+
+public static void main(String[] args) {
+		Configuration conf = new Configuration();
+		conf.set("fs.default.name","hdfs://localhost:9000");
+		String remoteFilePath = "wyr.txt";
+		// HDFS 路径
+		try {
+			System.out.println("读取文件信息: " + remoteFilePath);
+			HDFSapi.ls(conf, remoteFilePath);
+			System.out.println("\n 读取完成");
+			} catch (Exception e) {
+			e.printStackTrace();
+		      }
+		}
+}
